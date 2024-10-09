26da29e1297826a0600d498f945ac58cad9290ae
Cherry-16 <145067279+Cherry-16@users.noreply.github.com>
2024-10-08 20:27:45 -0600

    Avance practica 3.1

--- introLaravel/routes/web.php
+++ introLaravel/routes/web.php
@@ -16,4 +16,5 @@ Route::get('/form', function () {
 
         Route::view('/', 'Inicio')->name('rutaInicio');
         Route::view('/form', 'formulario')->name('rutaForm');
-        Route::view('/consultar', 'clientes')->name('rutaClientes');
+        Route::view('/consultar', 'clientes')->name('rutaClientes'); 
+        Route::view('/component', 'componentes')->name('rutaComponent');
