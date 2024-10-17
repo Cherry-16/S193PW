a96d4643a53a098e3a8e7811be587d24f42bd397
Cherry-16 <145067279+Cherry-16@users.noreply.github.com>
2024-10-17 13:34:54 -0600

    creacion_vista_repaso1
    Se crea la vista repaso1 y su controlador

--- practicaRepasos/routes/web.php
+++ practicaRepasos/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('inicio');
 })->name('rutainicio');
+
+Route::get('/repaso1', function () {
+    return view('repaso1'); 
+    })->name('rutarepaso1');
