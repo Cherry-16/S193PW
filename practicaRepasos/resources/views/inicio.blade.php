3c1d5e4de0eaf8399bd7f3908eb9a7a830f2f0b7
Cherry-16 <145067279+Cherry-16@users.noreply.github.com>
2024-10-17 13:24:01 -0600

    vistainicio_portada
    Se ingresan los datos en la vista inicio

---
+++ practicaRepasos/resources/views/inicio.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portada</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+    <div class="d-flex flex-column justify-content-center align-items-center text-center vh-100">
+        <h1 class="display-1">Universidad Politécnica de Querétaro</h1>
+        <img src="https://api.encuentraescuela.com.mx/storage/app/logos/1575402099_9561LOGO_UPQ-01.png" width="20%">
+        <br>
+        <p>
+            Nombre de la Alumna: Vanessa Sthephania Martínez Macías <br>
+            Nombre del Profesor: Ivan Isay Guerra Lopez <br>
+            Nombre de la materia: Programación web <br>
+            Fecha de entrega: 2024-10-17 <br>
+            Grupo: S-193
+        </p>
+        <a href="/repaso1" class="btn btn-success mt-3">Repaso 1</a>
+    </div>
+</body>
+</html> 
+
