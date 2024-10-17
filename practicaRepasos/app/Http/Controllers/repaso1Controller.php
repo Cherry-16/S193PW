85c0bb13b880b56560facf75da6c9587f81aa97b
Cherry-16 <145067279+Cherry-16@users.noreply.github.com>
2024-10-17 17:46:19 -0600

    Convertidor
    En el commit anterior se creó el controlador de las vistas y de las operaciones que realiza el convertidor, además se creó la ruta para mostrar el resultado en una cadena de texto

--- practicaRepasos/app/Http/Controllers/repaso1Controller.php
+++ practicaRepasos/app/Http/Controllers/repaso1Controller.php
@@ -6,17 +6,37 @@ use Illuminate\Http\Request;
 
 class repaso1Controller extends Controller
 {
-    
     public function home()
     {
         return view('inicio');
-    } 
+    }
+
     public function formulario()
     {
-        return view('repaso');
-    } 
-    public function Convertidor()
+        return view('repaso1');
+    }
+
+    public function convertidor(Request $request)
     {
-         return 'Se convirtió :)';
-}
+        $cantidad = $request->input('cantidad');
+        $unidad = $request->input('unidad');
+        $resultado = '';
+
+        switch ($unidad) {
+            case 'MBtoGB':
+                $resultado = $cantidad / 1024 . ' GB';
+                break;
+            case 'GBtoMB':
+                $resultado = $cantidad * 1024 . ' MB';
+                break;
+            case 'GBtoTB':
+                $resultado = $cantidad / 1024 . ' TB';
+                break;
+            case 'TBtoGB':
+                $resultado = $cantidad * 1024 . ' GB';
+                break;
+        }
+
+        return "El resultado de la conversión es: " . $resultado;
+    }
 }
