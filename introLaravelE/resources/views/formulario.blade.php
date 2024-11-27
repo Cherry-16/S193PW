b3198b297bf3fa299f2d50561900903339a6f5b8
Cherry-16 <145067279+Cherry-16@users.noreply.github.com>
2024-11-26 20:34:31 -0600

    BD_Eloquent

--- introLaravelE/resources/views/formulario.blade.php
+++ introLaravelE/resources/views/formulario.blade.php
@@ -30,7 +30,7 @@
 
     <div class="card-body text-justify ">
 
-      <form action="/enviarCliente" method="POST">
+      <form action="{{route('cliente.store')}}" method="POST">
         @csrf
 
         <div class="mb-3">
---
+++ introLaravelE/routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
+
+        // rutas para controlador
+        Route::get('/', [ClienteController::class, 'home'])->name('rutaInicio');
+        Route::resource('cliente', ClienteController::class); 
